9769f08d58d75f4d428e0868d287a8c8c2a52a4a
Luis Aguirre <luis@aguirrel.com.ar>
2014-05-09 14:43:10 -0300

    Adding coverage to test

--- test/karma.config.js
+++ test/karma.config.js
@@ -32,14 +32,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+        '../src/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
