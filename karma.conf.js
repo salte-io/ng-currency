e8994c74566e9056928dbd2f184b91a85893ca8f
Nick Woodward <nick@salte.io>
2016-07-27 19:07:29 -0500

    Update the build process to catch test and linting errors * Added a Webpack build process (and ES6 and module support) * Added Eslint and fixed some errors * Added an Editorconfig to enforce consistent style across the project * Moved angular to an external dependency

---
+++ karma.conf.js
@@ -0,0 +1,53 @@
+var webpackConfig = require('./webpack.test.config.js');
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+
+    frameworks: [
+      'jasmine'
+    ],
+
+    files: [
+      'test/index.js'
+    ],
+
+    preprocessors: {
+      'test/index.js': ['webpack', 'sourcemap'],
+      'src/**/*.js': ['coverage']
+    },
+
+    webpack: webpackConfig,
+
+    webpackMiddleware: {
+      noInfo: true
+    },
+
+    reporters: ['mocha', 'coverage'],
+
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        { type: 'text' },
+        { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
+      ]
+    },
+
+    port: 9876,
+
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    autoWatch: true,
+
+    browsers: ['PhantomJS'],
+
+    captureTimeout: 60000,
+    browserDisconnectTimeout: 7000,
+    browserDisconnectTolerance: 1,
+    browserDisconnectNoActivityTimeout: 60000,
+
+    singleRun: false
+  });
+};
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: './ng-currency.module.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'ng-currency.js',
+    sourceMapFilename: '[file].map',
+    library: 'ng-currency',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  externals: [{
+    angular: {
+      root: 'angular',
+      commonjs2: 'angular',
+      commonjs: 'angular',
+      amd: 'angular'
+    }
+  }],
+  devtool: 'source-map',
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'eslint'
+    }],
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'ng-annotate?map=false!babel'
+    }]
+  }
+};
