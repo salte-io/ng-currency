2403e6eb9ba2c8cd6987da2bfc0d34b6894e7d9d
Ceci <admin@cecilias.me>
2017-03-20 08:14:56 -0500

    feat: Add the ability to configure the default binding values (#135)
    closes #134 and closes #121

--- src/ng-currency.module.js
+++ src/ng-currency.module.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
+import ngCurrencySettings from './ng-currency-settings.provider.js';
 import ngCurrency from './ng-currency.directive.js';
 
 const module = angular.module('ng-currency', []);
 
+module.provider('ngCurrencySettings', ngCurrencySettings);
 module.directive('ngCurrency', ngCurrency);
 
 export default module.name;
